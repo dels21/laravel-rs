73d8c34255b98f8d1a85420f8ca666f40d32560b
DexterVK <dexter.krisnadhi@binus.ac.id>
2024-05-24 22:00:56 +0700

    Login views + css

--- horse/resources/views/authentication/login.blade.php
+++ horse/resources/views/authentication/login.blade.php
@@ -7,60 +7,56 @@
 
     <!-- Bootstrap CSS -->
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.4.1/dist/css/bootstrap.min.css" integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
-
-    <title>Hello, world!</title>
+    <link rel="stylesheet" href="">
+     {{--rencananya masukin css login disini  --}}
+    <title>Login</title>
   </head>
   <body>
-    <section class="vh-100" style="background-color: #9A616D;">
-        <div class="container py-5 h-100">
-          <div class="row d-flex justify-content-center align-items-center h-100">
-            <div class="col col-xl-10">
-              <div class="card" style="border-radius: 1rem;">
-                <div class="row g-0">
-                  <div class="col-md-6 col-lg-5 d-none d-md-block">
-                    <img src="https://mdbcdn.b-cdn.net/img/Photos/new-templates/bootstrap-login-form/img1.webp"
-                      alt="login form" class="img-fluid" style="border-radius: 1rem 0 0 1rem;" />
-                  </div>
-                  <div class="col-md-6 col-lg-7 d-flex align-items-center">
-                    <div class="card-body p-4 p-lg-5 text-black">
+    <section class="gradient-form" style="background-color: #eee;">
+              <div class="card rounded-3 text-black container-fluid h-100">
+                <div class="row g-0 h-100">
+                  <img src="https://letsenhance.io/static/8f5e523ee6b2479e26ecc91b9c25261e/1015f/MainAfter.jpg" class="col-lg-6 " style="object-fit: cover;padding: 0;">
+                    
+                  <div class="col-lg-6">
+                    <div class="card-body p-md-5 mx-md-4">
+      
+                      <div class="text-center">
+                        <img src="https://mdbcdn.b-cdn.net/img/Photos/new-templates/bootstrap-login-form/lotus.webp"
+                          style="width: 185px;" alt="logo">
+                        <h4 class="mt-1 mb-5 pb-1">Reinert Hospital</h4>
+                      </div>
       
                       <form>
       
-                        <div class="d-flex align-items-center mb-3 pb-1">
-                          <i class="fas fa-cubes fa-2x me-3" style="color: #ff6219;"></i>
-                          <span class="h1 fw-bold mb-0">Logo</span>
-                        </div>
-      
-                        <h5 class="fw-normal mb-3 pb-3" style="letter-spacing: 1px;">Sign into your account</h5>
-      
                         <div data-mdb-input-init class="form-outline mb-4">
-                          <input type="email" id="form2Example17" class="form-control form-control-lg" />
-                          <label class="form-label" for="form2Example17">Email address</label>
+                            <label class="form-label" for="form2Example11">Email</label>
+                          <input type="email" id="form2Example11" class="form-control"
+                            placeholder="Masukkan alamat email anda disini" />
                         </div>
       
                         <div data-mdb-input-init class="form-outline mb-4">
-                          <input type="password" id="form2Example27" class="form-control form-control-lg" />
-                          <label class="form-label" for="form2Example27">Password</label>
+                            <label class="form-label" for="form2Example22">Password</label>
+                          <input type="password" id="form2Example22" class="form-control" 
+                          placeholder="Masukkan password anda disini"/>
                         </div>
       
-                        <div class="pt-1 mb-4">
-                          <button data-mdb-button-init data-mdb-ripple-init class="btn btn-dark btn-lg btn-block" type="button">Login</button>
+                        <div class="text-center pt-1 mb-5 pb-1">
+                          <button data-mdb-button-init data-mdb-ripple-init class="btn btn-primary btn-block fa-lg mb-3" type="button">Login</button>
+                          <!-- <a class="text-muted" href="#!">Forgot password?</a> -->
                         </div>
       
-                        <a class="small text-muted" href="#!">Forgot password?</a>
-                        <p class="mb-5 pb-lg-2" style="color: #393f81;">Don't have an account? <a href="#!"
-                            style="color: #393f81;">Register here</a></p>
-                        <a href="#!" class="small text-muted">Terms of use.</a>
-                        <a href="#!" class="small text-muted">Privacy policy</a>
+                        <div class="d-flex align-items-center justify-content-center pb-4 ">
+                          <p class="mb-0 mr-3">Tidak punya akun?</p>
+                          <button  type="button" data-mdb-button-init data-mdb-ripple-init class="btn btn-outline-danger">Buat akun</button>
+                        </div>
+      
                       </form>
       
                     </div>
                   </div>
+                  
                 </div>
               </div>
-            </div>
-          </div>
-        </div>
       </section>
     <script src="https://code.jquery.com/jquery-3.4.1.slim.min.js" integrity="sha384-J6qa4849blE2+poT4WnyKhv5vZF5SrPo0iEjwBvKU7imGFAV0wwj1yYfoRSJoZ+n" crossorigin="anonymous"></script>
     <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js" integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo" crossorigin="anonymous"></script>
