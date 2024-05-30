6218f3208b95440cbcc2132462c092b4126fbfe9
DexterVK <dexter.krisnadhi@binus.ac.id>
2024-05-30 11:00:53 +0700

    Middleware (after merge)

--- horse/app/Http/Controllers/Auth/AuthenticatedSessionController.php
+++ horse/app/Http/Controllers/Auth/AuthenticatedSessionController.php
@@ -27,8 +27,8 @@ class AuthenticatedSessionController extends Controller
         $request->authenticate();
 
         $request->session()->regenerate();
-
-        return redirect()->intended(route('dashboard', absolute: false));
+        $role = Auth::user()->role;
+        return redirect()->intended("/{$role}/dashboard");
     }
 
     /**
