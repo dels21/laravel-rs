5d941939bc5d91d00d736847ae84d250cebe0ed3
keyforedu <keyforedu@gmail.com>
2024-07-09 15:27:00 +0700

    fix table layout, sisa edit di detail

--- horse/resources/views/pasien/detail-pemeriksaan-pasien.blade.php
+++ horse/resources/views/pasien/detail-pemeriksaan-pasien.blade.php
@@ -1,6 +1,6 @@
 @extends('layouts.app')
 @section('title', 'Detail Pemeriksaan Saya')
-@section('setAktifDetailPemeriksaanSaya', 'active')
+@section('setAktifPemeriksaanSaya', 'active')
 
 @section('customCSS')
 
@@ -13,27 +13,34 @@
 @endsection
 
 @section('content')
-<div class="container">
-    <h1 class="biggest-font mt-5 mb-5">Detail Pemeriksaan Saya</h1>
-    @if ($detail->isEmpty())
-        <p>Belum ada detail pemeriksaan.</p>
-    @else
+
+    <!-- Begin Page Content -->
+    <div class="container-fluid">
+
+        <!-- Page Heading -->
+        <h1 class="biggest-font mt-5 mb-5">Detail Pemeriksaan Saya</h1>
+        @if ($detail->isEmpty())
+            <p>Belum ada detail pemeriksaan.</p>
+            {{-- <p>{{ $loggedInUserId }}</p> --}}
+            @else
+        <!-- DataTales Example -->
         <div class="card shadow mb-4">
-            <div class="table-responsive">
-                <table class="table table-bordered" width="100%" cellspacing="0">
-                    <thead>
-                        <tr>
-                            <th>No</th>
-                            <th>No. Pemeriksaan</th>
-                            <th>Tanggal Pemeriksaan</th>
-                            <th>Jam Mulai Pemeriksaan Alat</th>
-                            <th>Jam Selesai Pemeriksaan Alat</th>
-                            <th>Ruangan</th>
-                            <th>Status</th>
-                        </tr>
-                    </thead>
-                    <tbody>
-                        @foreach ($detail as $item)
+            <div class="card-body">
+                <div class="table-responsive">
+                    <table class="table table-bordered" id="dataTable" width="100%" cellspacing="0">
+                        <thead>
+                            <tr>
+                                <th>No</th>
+                                <th>No. Pemeriksaan</th>
+                                <th>Tanggal Pemeriksaan</th>
+                                <th>Jam Mulai Pemeriksaan Alat</th>
+                                <th>Jam Selesai Pemeriksaan Alat</th>
+                                <th>Ruangan</th>
+                                <th>Status</th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            @foreach ($detail as $item)
                             <tr>
                                 <td>{{ $loop->iteration }}</td>
                                 <td>{{ $item->nomorPemeriksaan }}</td>
@@ -44,11 +51,31 @@
                                 <td>{{ $item->status }}</td>
                             </tr>
                         @endforeach
-                    </tbody>
-                </table>
+                        </tbody>
+                    </table>
+                </div>
             </div>
-            {{ $detail->links() }}
-        </div>
-    @endif
-</div>
+        @endif
+    </div>
+@endsection
+
+@section('customJS')
+
+    <!-- Bootstrap core JavaScript-->
+    <script src="/templating-assets/vendor/jquery/jquery.min.js"></script>
+    <script src="/templating-assets/vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
+
+    <!-- Core plugin JavaScript-->
+    <script src="/templating-assets/vendor/jquery-easing/jquery.easing.min.js"></script>
+
+    <!-- Custom scripts for all pages-->
+    <script src="/templating-assets/js/sb-admin-2.min.js"></script>
+
+    <!-- Page level plugins -->
+    <script src="/templating-assets/vendor/datatables/jquery.dataTables.min.js"></script>
+    <script src="/templating-assets/vendor/datatables/dataTables.bootstrap4.min.js"></script>
+
+    <!-- Page level custom scripts -->
+    <script src="/templating-assets/js/demo/datatables-demo.js"></script>
+
 @endsection
