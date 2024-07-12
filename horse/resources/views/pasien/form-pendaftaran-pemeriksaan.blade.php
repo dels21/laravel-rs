bcee66910d70ff2de8c55ddfd3b790b4b3ba6e1c
dels21 <aderinukharisma21@gmail.com>
2024-07-12 22:10:46 +0700

    Update form-pendaftaran-pemeriksaan.blade.php

--- horse/resources/views/pasien/form-pendaftaran-pemeriksaan.blade.php
+++ horse/resources/views/pasien/form-pendaftaran-pemeriksaan.blade.php
@@ -11,63 +11,16 @@
             <div class="row">
                 <div class="col-md-6">
                     <div class="mb-3">
-                        <label for="noPemeriksaan" class="form-label">No. Pemeriksaan</label>
+                        <label for="noPemeriksaan" class="form-label">No. Pendaftaran</label>
                         <input type="text" class="form-control" id="noPemeriksaan" name="noPemeriksaan" disabled>
                     </div>
                     <div class="mb-3">
-                        <label for="namaPasien" class="form-label">Nama Pasien</label>
-                        <input type="text" class="form-control" id="namaPasien" name="namaPasien">
+                        <label for="idPasien" class="form-label">Pasien</label>
+                        <input type="text" class="form-control" id="idPasien" name="idPasien">
                     </div>
                     <div class="mb-3">
-                        <label for="tanggalLahir" class="form-label">Tanggal Lahir</label>
-                        <input type="date" class="form-control" id="tanggalLahir" name="tanggalLahir">
-                    </div>
-                    <div class="mb-3">
-                        <label for="jenisKelamin" class="form-label">Jenis Kelamin</label> <br>
-                        <div class="d-flex">
-                            <div class="form-check me-3">
-                                <input class="form-check-input" type="radio" id="jenisKelaminLaki" name="jenisKelaminPemohon"
-                                    value="L" checked>
-                                <label class="form-check-label" for="jenisKelaminLaki">Laki-laki</label>
-                            </div>
-                            <div class="form-check">
-                                <input class="form-check-input" type="radio" id="jenisKelaminPerempuan"
-                                    name="jenisKelaminPemohon" value="P">
-                                <label class="form-check-label" for="jenisKelaminPerempuan">Perempuan</label>
-                            </div>
-                        </div>
-                    </div>
-                    <div class="mb-3">
-                        <label for="alamat" class="form-label">Alamat</label>
-                        <textarea class="form-control" id="alamat" name="alamat" rows="3"></textarea>
-                    </div>
-                    <div class="mb-3">
-                        <label for="Permohonan Permintaan" class="form-label">Permohonan Permintaan</label> <br>
-                        <div class="d-flex">
-                            <div class="form-check me-3">
-                                <input class="form-check-input" type="radio" id="jenisKelaminPemohonLaki"
-                                    name="jenisKelaminPemohon" value="LPemohon" checked>
-                                <label class="form-check-label" for="jenisKelaminPemohonLaki">Laki-laki</label>
-                            </div>
-                            <div class="form-check">
-                                <input class="form-check-input" type="radio" id="jenisKelaminPemohonPerempuan"
-                                    name="jenisKelaminPemohon" value="PPemohon">
-                                <label class="form-check-label" for="jenisKelaminPemohonPerempuan">Perempuan</label>
-                            </div>
-                        </div>
-                    </div>
-                    <div class="mb-3">
-                        <label for="Permohonan Hasil" class="form-label">Permohonan Hasil</label> <br>
-                        <div class="form-check">
-                            <input class="form-check-input" type="radio" id="hasilKeDokter" name="hasilKeDokter"
-                                value="hasilKeDokter" checked>
-                            <label class="form-check-label" for="hasilKeDokter">Hasil diserahkan ke dokter</label>
-                        </div>
-                        <div class="form-check">
-                            <input class="form-check-input" type="radio" id="hasilKePasien" name="hasilKePasien"
-                                value="hasilKePasien">
-                            <label class="form-check-label" for="hasilKePasien">Hasil diserahkan ke pasien</label>
-                        </div>
+                        <label for="attachment" class="form-label">Attachment</label>
+                        <input type="file" class="form-control" id="attachment" name="attachment">
                     </div>
                 </div>
                 <div class="col-md-6">
@@ -76,32 +29,8 @@
                         <input type="date" class="form-control" id="tanggal" name="tanggal">
                     </div>
                     <div class="mb-3">
-                        <label for="teleponMobile" class="form-label">Telepon Mobile</label>
-                        <input type="text" class="form-control" id="teleponMobile" name="teleponMobile">
-                    </div>
-                    <div class="mb-3">
-                        <label for="teleponDarurat" class="form-label">Telepon Darurat</label>
-                        <input type="text" class="form-control" id="teleponDarurat" name="teleponDarurat">
-                    </div>
-                    <div class="mb-3">
-                        <label for="dokterPengirim" class="form-label">Dokter Pengirim</label>
-                        <input type="text" class="form-control" id="dokterPengirim" name="dokterPengirim">
-                    </div>
-                    <div class="mb-3">
-                        <label for="dokterPengirim" class="form-label">Dokter Pengirim</label>
-                        <input type="text" class="form-control" id="dokterPengirim" name="dokterPengirim">
-                    </div>
-                    <div class="mb-3">
-                        <label for="ruangan" class="form-label">Ruangan</label>
-                        <input type="text" class="form-control" id="ruangan" name="ruangan">
-                    </div>
-                    <div class="mb-3">
-                        <label for="teleponDokter" class="form-label">Telepon Dokter</label>
-                        <input type="text" class="form-control" id="teleponDokter" name="teleponDokter">
-                    </div>
-                    <div class="mb-3">
-                        <label for="diagnosa" class="form-label">Diagnosa</label>
-                        <input type="text" class="form-control" id="diagnosa" name="diagnosa">
+                        <label for="idDokter" class="form-label">Dokter Pengirim</label>
+                        <input type="text" class="form-control" id="idDokter" name="idDokter">
                     </div>
                 </div>
             </div>
