5b84ecc4af982b5a0885336f4d6a40268d8e3031
dels21 <aderinukharisma21@gmail.com>
2024-07-13 13:32:15 +0700

    Update form-pendaftaran-pemeriksaan.blade.php

--- horse/resources/views/pasien/form-pendaftaran-pemeriksaan.blade.php
+++ horse/resources/views/pasien/form-pendaftaran-pemeriksaan.blade.php
@@ -11,12 +11,8 @@
             <div class="row">
                 <div class="col-md-6">
                     <div class="mb-3">
-                        <label for="noPemeriksaan" class="form-label">No. Pendaftaran</label>
-                        <input type="text" class="form-control" id="noPemeriksaan" name="noPemeriksaan" disabled>
-                    </div>
-                    <div class="mb-3">
-                        <label for="idPasien" class="form-label">Pasien</label>
-                        <input type="text" class="form-control" id="idPasien" name="idPasien">
+                        <label for="tanggal" class="form-label">Tanggal</label>
+                        <input type="date" class="form-control" id="tanggal" name="tanggal">
                     </div>
                     <div class="mb-3">
                         <label for="attachment" class="form-label">Attachment</label>
@@ -25,11 +21,7 @@
                 </div>
                 <div class="col-md-6">
                     <div class="mb-3">
-                        <label for="tanggal" class="form-label">Tanggal</label>
-                        <input type="date" class="form-control" id="tanggal" name="tanggal">
-                    </div>
-                    <div class="mb-3">
-                        <label for="idDokter" class="form-label">Dokter Pengirim</label>
+                        <label for="idDokter" class="form-label">Nama Dokter Pengirim</label>
                         <input type="text" class="form-control" id="idDokter" name="idDokter">
                     </div>
                 </div>
@@ -37,65 +29,63 @@
 
             <hr>
 
-            <div class="row">
-                <div class="col-8">
-                    <div class="mb-3">
-                        <label for="modalitas" class="form-label">Modalitas</label>
-                        <select class="form-select" id="modalitas" name="modalitas" aria-label="Pilih modalitas">
-                            <option value="">Pilih modalitas</option>
-                            <option value="MRI">MRI</option>
-                            <option value="CT Scan">CT Scan</option>
-                            <option value="Ultrasound">Ultrasound</option>
-                            <option value="Rontgen">Rontgen</option>
-                        </select>
+            <div class="container mt-5">
+                <div class="row" id="form-container">
+                    <div class="col-8">
+                        <div class="mb-3">
+                            <label for="modalitas1" class="form-label">Modalitas</label>
+                            <select class="form-select" id="modalitas1" name="modalitas[]" aria-label="Pilih modalitas">
+                                <option value="">Pilih modalitas</option>
+                                <option value="MRI">MRI</option>
+                                <option value="CT Scan">CT Scan</option>
+                                <option value="Ultrasound">Ultrasound</option>
+                                <option value="Rontgen">Rontgen</option>
+                            </select>
+                        </div>
                     </div>
-                    <div class="mb-3">
-                        <label for="modalitas" class="form-label">Modalitas</label>
-                        <select class="form-select" id="modalitas" name="modalitas" aria-label="Pilih modalitas">
-                            <option value="">Pilih modalitas</option>
-                            <option value="MRI">MRI</option>
-                            <option value="CT Scan">CT Scan</option>
-                            <option value="Ultrasound">Ultrasound</option>
-                            <option value="Rontgen">Rontgen</option>
-                        </select>
-                    </div>
-                    <div class="mb-3">
-                        <label for="modalitas" class="form-label">Modalitas</label>
-                        <select class="form-select" id="modalitas" name="modalitas" aria-label="Pilih modalitas">
-                            <option value="">Pilih modalitas</option>
-                            <option value="MRI">MRI</option>
-                            <option value="CT Scan">CT Scan</option>
-                            <option value="Ultrasound">Ultrasound</option>
-                            <option value="Rontgen">Rontgen</option>
-                        </select>
+                    <div class="col-4">
+                        <div class="mb-3">
+                            <label for="harga1" class="form-label">Harga</label>
+                            <input type="number" class="form-control" id="harga1" name="harga[]" min="0"
+                                placeholder="Masukkan harga modalitas">
+                        </div>
                     </div>
                 </div>
-
-                <div class="col-4">
-                    <div class="mb-3">
-                        <label for="harga" class="form-label">Harga</label>
-                        <input type="number" class="form-control" id="harga" name="harga" min="0"
-                            placeholder="Masukkan harga modalitas">
-                    </div>
-                    <div class="mb-3">
-                        <label for="harga" class="form-label">Harga</label>
-                        <input type="number" class="form-control" id="harga" name="harga" min="0"
-                            placeholder="Masukkan harga modalitas">
-                    </div>
-                    <div class="mb-3">
-                        <label for="harga" class="form-label">Harga</label>
-                        <input type="number" class="form-control" id="harga" name="harga" min="0"
-                            placeholder="Masukkan harga modalitas">
-                    </div>
-                </div>
-
-            </div>
-            <div class="row my-2">
-                <div class="col d-flex justify-content-center">
-                    <button type="submit" class="btn btn-primary">Submit</button>
-                </div>
+                <button id="add-more-btn" type="button" class="btn btn-primary">Tambah Pemeriksaan</button>
             </div>
 
-        </form>
+            <script>
+                document.getElementById('add-more-btn').addEventListener('click', function() {
+                    // Get the container where the form rows are added
+                    var container = document.getElementById('form-container');
+
+                    // Clone the first row (both columns)
+                    var firstRow = container.parentElement.children[0];
+                    var newRow = firstRow.cloneNode(true);
+
+                    // Update the id for the new dropdown and text field
+                    var rowCount = container.parentElement.children.length;
+                    var newModalitasId = 'modalitas' + rowCount;
+                    var newHargaId = 'harga' + rowCount;
+
+                    newRow.querySelector('label[for="modalitas1"]').setAttribute('for', newModalitasId);
+                    newRow.querySelector('select#modalitas1').setAttribute('id', newModalitasId);
+
+                    newRow.querySelector('label[for="harga1"]').setAttribute('for', newHargaId);
+                    newRow.querySelector('input#harga1').setAttribute('id', newHargaId);
+
+                    // Append the new row to the container
+                    container.parentNode.insertBefore(newRow, container.nextSibling);
+                });
+            </script>
+
+    </div>
+    <div class="row mt-5">
+        <div class="col d-flex justify-content-center">
+            <button type="submit" class="btn btn-primary">Submit</button>
+        </div>
+    </div>
+
+    </form>
     </div>
 @endsection
