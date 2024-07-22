be83d5015cf90d1dcda6bfb4644f9139a8fe03cf
keyforedu <keyforedu@gmail.com>
2024-07-22 15:07:35 +0700

    list pemeriksaan done

--- horse/resources/views/pasien/list-pemeriksaan-pasien.blade.php
+++ horse/resources/views/pasien/list-pemeriksaan-pasien.blade.php
@@ -13,59 +13,6 @@
 @endsection
 
 @section('content')
-{{-- 
-       <!-- Modal -->
-    <div class="modal fade bd-example-modal-xl" id="myModal" tabindex="-1" role="dialog"
-        aria-labelledby="myExtraLargeModalLabel" aria-hidden="true">
-        <div class="modal-dialog modal-xl">
-            <div class="modal-content">
-                <div class="modal-header">
-                    <h1 class="h1-title-600 w-100 text-center" id="myExtraLargeModalLabel">Detail Pemeriksaan</h1>
-                    <button type="button" class="close" data-dismiss="modal" aria-label="Close">
-                        <span aria-hidden="true">&times;</span>
-                    </button>
-                </div>
-                <div class="modal-body">
-                    <!-- DataTales Example -->
-                    <div class="card shadow mb-4">
-                        <div class="card-body">
-                            <div class="table-responsive">
-                                <table class="table" width="100%" cellspacing="0">
-                                    <thead>
-                                        <tr>
-                                            <th>No</th>
-                                            <th>No. Pemeriksaan</th>
-                                            <th>Tanggal Pemeriksaan</th>
-                                            <th>Jam Mulai Pemeriksaan</th>
-                                            <th>Jam Selesai Pemeriksaan</th>
-                                            <th>Ruangan</th>
-                                            <th>Status</th>
-                                            <th>Action</th>
-                                        </tr>
-                                    </thead>
-                                    <tbody>
-                                        @foreach ($data as $item)
-                                        <tr>
-                                            <td>{{$loop->iteration}}</td>
-                                            <td>{{$item->noPemeriksaan}}</td>
-                                            <td>{{$item->tanggal}}</td>
-                                            <td>{{$item->jamMulai}}</td>
-                                            <td>{{$item->jamSelesai}}</td>
-                                            <td>{{$item->ruangan}}</td>
-                                            <td>{{$item->status}}</td> --}}
-                                            {{-- <td><a class="detail-link" data-toggle="modal" data-target="#myModal">Detail</a></td>   --}}
-                                        {{-- </tr>
-                                        @endforeach
-                                    </tbody>
-                                </table>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-            </div>
-        </div>
-    </div> --}}
-
     <!-- Begin Page Content -->
     <div class="container-fluid">
 
